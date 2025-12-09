f29d677742151f0fab3ddb8a8fe0d4a01749d84d
Christoph Schniedermeier <zdxerr@gmail.com>
2025-12-09 14:19:45 +0100

    Update 25.8 + 25.9.

--- src/y2025/d08.rs
+++ src/y2025/d08.rs
@@ -1,13 +1,11 @@
 use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 use std::{fs, usize};
-const START: char = 'S';
-use std::collections::BinaryHeap;
 pub fn part1(input_path: &PathBuf) -> Result<usize, Box<dyn std::error::Error>> {
-    // let content = fs::read(input_path)?;
     let file = File::open(input_path)?;
     let reader = BufReader::with_capacity(1024, file);
 
@@ -30,10 +28,10 @@ pub fn part1(input_path: &PathBuf) -> Result<usize, Box<dyn std::error::Error>> 
         BinaryHeap::with_capacity(positions.len().pow(2));
 
     for a in 0..positions.len() {
-        for b in a..positions.len() {
-            if a == b {
-                continue;
-            }
+        for b in (a + 1)..positions.len() {
+            // if a == b {
+            //     continue;
+            // }
             let (ax, ay, az) = positions[a];
             let (bx, by, bz) = positions[b];
 
@@ -53,8 +51,6 @@ pub fn part1(input_path: &PathBuf) -> Result<usize, Box<dyn std::error::Error>> 
 
         let posa = positions[a];
         let posb = positions[b];
-
-        // println!("{distance:?} {posa:?} {posb:?}");
 
         let ua = unions[a];
         let ub = unions[b];
@@ -76,7 +72,6 @@ pub fn part1(input_path: &PathBuf) -> Result<usize, Box<dyn std::error::Error>> 
         unions[a] = parent;
         unions[b] = parent;
     }
-    // dbg!(&unions);
     let mut unions2: HashMap<&usize, usize> = HashMap::new();
     for u in unions.iter().filter(|n| **n != usize::MAX) {
         unions2
@@ -89,7 +84,6 @@ pub fn part1(input_path: &PathBuf) -> Result<usize, Box<dyn std::error::Error>> 
 
     let mut unions2: Vec<&usize> = unions2.values().collect();
     unions2.sort_unstable();
-    // dbg!(&unions2);
 
     let result = unions2
         .into_iter()
@@ -100,8 +94,90 @@ pub fn part1(input_path: &PathBuf) -> Result<usize, Box<dyn std::error::Error>> 
     Ok(result)
 }
 
-pub fn part2(input_path: &PathBuf) -> Result<usize, Box<dyn std::error::Error>> {
+pub fn part2(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
     let content = fs::read(input_path)?;
 
-    Ok(0)
+    let positions: Vec<(u64, u64, u64)> = content
+        .lines()
+        .map(|line| line.unwrap())
+        .map(|line| {
+            let mut splitted = line
+                .splitn(3, ',')
+                .map(|number| number.parse::<u64>().unwrap());
+            (
+                splitted.next().unwrap(),
+                splitted.next().unwrap(),
+                splitted.next().unwrap(),
+            )
+        })
+        .collect();
+
+    let mut distances: BinaryHeap<(Reverse<u64>, (usize, usize))> =
+        BinaryHeap::with_capacity(positions.len().pow(2));
+
+    for a in 0..positions.len() {
+        for b in (a + 1)..positions.len() {
+            let (ax, ay, az) = positions[a];
+            let (bx, by, bz) = positions[b];
+
+            let x = ax.abs_diff(bx);
+            let y = ay.abs_diff(by);
+            let z = az.abs_diff(bz);
+
+            distances.push((Reverse(x * x + y * y + z * z), (a, b)));
+        }
+    }
+
+    let mut unions: Vec<usize> = vec![usize::MAX; positions.len()];
+
+    let mut count: HashMap<usize, usize> = HashMap::with_capacity(100);
+
+    loop {
+        let (distance, (a, b)) = distances.pop().unwrap();
+
+        let parent = match (unions[a], unions[b]) {
+            (usize::MAX, usize::MAX) => {
+                let k = a.min(b);
+                count.insert(k, 2);
+                k
+            }
+            (n, usize::MAX) | (usize::MAX, n) => {
+                count.entry(n).and_modify(|c| *c += 1);
+                n
+            }
+            (parent_a, parent_b) => {
+                if parent_a == parent_b {
+                    parent_a
+                } else {
+                    let parent = parent_a.min(parent_b);
+                    let old = parent_a.max(parent_b);
+                    unions
+                        .iter_mut()
+                        .filter(|value| **value == old)
+                        .for_each(|value| {
+                            count.entry(parent).and_modify(|c| *c += 1);
+                            *value = parent;
+                        });
+                    count.remove(&old);
+                    parent
+                }
+            }
+        };
+
+        unions[a] = parent;
+        unions[b] = parent;
+
+        if *count.get(&parent).unwrap() >= positions.len() {
+            let a = positions[a];
+            let b = positions[b];
+            return Ok(a.0 * b.0);
+        }
+
+        let groups: Vec<(usize, &usize)> = unions
+            .iter()
+            .enumerate()
+            .filter(|(_, v)| **v != usize::MAX)
+            .collect();
+    }
+    unreachable!();
 }
---
+++ src/y2025/d09.rs
@@ -0,0 +1,45 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+use std::collections::{HashMap, HashSet};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
+use std::{fs, usize};
+pub fn part1(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
+    let content = fs::read(input_path)?;
+    let positions: Vec<(u64, u64)> = content
+        .lines()
+        .map(|line| line.unwrap())
+        .map(|line| {
+            let mut splitted = line
+                .splitn(2, ',')
+                .map(|number| number.parse::<u64>().unwrap());
+            (splitted.next().unwrap(), splitted.next().unwrap())
+        })
+        .collect();
+
+    // println!("{positions:?}");
+
+    let len = positions.len();
+
+    let mut largest_area = 0;
+    for a in 0..len {
+        for b in a + 1..len {
+            // print!("{a}/{b}");
+
+            let (a, b) = (positions[a], positions[b]);
+            let x = a.0.abs_diff(b.0) + 1;
+            let y = a.1.abs_diff(b.1) + 1;
+            let area = x * y;
+            largest_area = largest_area.max(area);
+        }
+    }
+
+    Ok(largest_area)
+}
+
+pub fn part2(input_path: &PathBuf) -> Result<usize, Box<dyn std::error::Error>> {
+    let content = fs::read(input_path)?;
+
+    Ok(0)
+}
