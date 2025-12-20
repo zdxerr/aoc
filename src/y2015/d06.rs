ba32cdd4ead0995a9c7e500bf08b3792b1f34908
Christoph Schniedermeier <zdxerr@gmail.com>
2025-12-20 12:36:54 +0100

    15.6

--- src/y2015/d06.rs
+++ src/y2015/d06.rs
@@ -6,6 +6,10 @@ use std::path::PathBuf;
 pub fn part1(input_path: &PathBuf) -> Result<usize, Box<dyn std::error::Error>> {
     let reader = BufReader::new(File::open(input_path)?);
     reader.bytes();
+
+    let mut grid = [false; 1000 * 1000];
+
+    let s: u64 = grid.iter().map(|v| if *v { 1 } else { 0 }).sum();
     Err("not implemented".into())
 }
 
