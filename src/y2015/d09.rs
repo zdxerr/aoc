2460b27df05434ea3adccb3267a25616365042a8
Christoph Schniedermeier <zdxerr@gmail.com>
2025-12-21 12:03:36 +0100

    Update 9 & 12.

--- src/y2015/d09.rs
+++ src/y2015/d09.rs
@@ -35,36 +35,75 @@ pub fn part1(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
     let mut path: HashSet<&str> = HashSet::with_capacity(8);
 
     fn walk<'a>(
-        current: &'a str,
+        left: &'a str,
+        right: &'a str,
         path: &mut HashSet<&'a str>,
         distance: u64,
         distances: &HashMap<&'a str, HashMap<&'a str, u64>>,
     ) -> u64 {
-        path.insert(current);
+        // println!("{left}-{right} [{path:?}] ({distance})");
+        // path.insert(current);
         let new_distance = if path.len() == distances.len() {
             distance
         } else {
-            distances[current]
-                .iter()
-                .filter_map(|(next, next_distance)| {
-                    if path.contains(next) {
-                        None
-                    } else {
-                        Some(walk(next, path, distance + next_distance, distances))
+            let mut new_distance = u64::MAX;
+            for key in distances.keys() {
+                // println!("{left} / {key}");
+                // let a = distances[left][key];
+                // let b = distances[right][key];
+                //
+                if path.contains(key) {
+                    continue;
+                }
+
+                match (distances[left].get(key), distances[right].get(key)) {
+                    (Some(a), Some(b)) => {
+                        if a < b {
+                            path.insert(key);
+                            new_distance =
+                                new_distance.min(walk(key, right, path, distance + a, distances));
+                            path.remove(key);
+                        } else {
+                            path.insert(key);
+                            new_distance =
+                                new_distance.min(walk(left, key, path, distance + b, distances));
+                            path.remove(key);
+                        }
                     }
-                })
-                .min()
-                .unwrap()
+                    (Some(a), None) => {
+                        path.insert(key);
+                        new_distance =
+                            new_distance.min(walk(key, right, path, distance + a, distances));
+                        path.remove(key);
+                    }
+                    (None, Some(b)) => {
+                        path.insert(key);
+                        new_distance =
+                            new_distance.min(walk(left, key, path, distance + b, distances));
+                        path.remove(key);
+                    }
+                    _ => (),
+                }
+            }
+            // distances[current]
+            //     .iter()
+            //     .filter_map(|(next, next_distance)| {
+            //         if path.contains(next) {
+            //             None
+            //         } else {
+            //             Some(walk(next, path, distance + next_distance, distances))
+            //         }
+            //     })
+            //     .min()
+            //     .unwrap()
+            new_distance
         };
-        path.remove(current);
+        // path.remove(current);
         new_distance
     }
-
-    Ok(distances
-        .keys()
-        .map(|start| walk(start, &mut path, 0, &distances))
-        .min()
-        .unwrap())
+    let start = distances.keys().next().unwrap();
+    path.insert(start);
+    Ok(walk(start, start, &mut path, 0, &distances))
 }
 
 pub fn part2(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
--- src/y2015/d12.rs
+++ src/y2015/d12.rs
@@ -2,9 +2,36 @@ use std::fs::File;
 use std::io::{BufReader, Read};
 use std::path::PathBuf;
 
-pub fn part1(input_path: &PathBuf) -> Result<usize, Box<dyn std::error::Error>> {
+pub fn part1(input_path: &PathBuf) -> Result<i64, Box<dyn std::error::Error>> {
     let reader = BufReader::new(File::open(input_path)?);
-    Err("not implemented".into())
+
+    let mut n: Option<u64> = None;
+    let mut neg: i8 = 1;
+    let mut sum = 0;
+    // println!();
+    for b in reader.bytes() {
+        let b = b.unwrap();
+
+        let d = b - b'0';
+
+        if d < 10 {
+            let n = n.get_or_insert(0);
+            *n = *n * 10 + d as u64;
+        } else if b == b'-' {
+            neg = -1;
+        } else {
+            if let Some(v) = n {
+                let v = v as i64 * neg as i64;
+                // print!("{v} ");
+                sum += v;
+                n = None;
+            }
+            neg = 1;
+        }
+    }
+    // dbg!(b'-', b'0');
+    // println!();
+    Ok(sum)
 }
 
 pub fn part2(_input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
