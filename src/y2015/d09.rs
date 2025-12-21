dece3d1ddf768fc10624228ca2b4d1411dd605cf
Christoph Schniedermeier <zdxerr@gmail.com>
2025-12-21 21:03:21 +0100

    2025 9+10

--- src/y2015/d09.rs
+++ src/y2015/d09.rs
@@ -1,10 +1,9 @@
 use std::collections::{HashMap, HashSet};
+use std::ffi::os_str::Display;
 use std::fs;
 use std::path::PathBuf;
 
-pub fn part1(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
-    let content = fs::read_to_string(input_path)?;
-
+fn parse(content: &str) -> HashMap<&str, HashMap<&str, u64>> {
     let mut distances: HashMap<&str, HashMap<&str, u64>> = HashMap::with_capacity(8);
     content.lines().for_each(|line| {
         let mut parts = line.split(' ');
@@ -31,6 +30,12 @@ pub fn part1(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
             .entry(from)
             .insert_entry(distance);
     });
+    distances
+}
+
+pub fn part1(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
+    let content = fs::read_to_string(input_path)?;
+    let distances = parse(&content);
 
     let mut path: HashSet<&str> = HashSet::with_capacity(8);
 
@@ -41,64 +46,33 @@ pub fn part1(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
         distance: u64,
         distances: &HashMap<&'a str, HashMap<&'a str, u64>>,
     ) -> u64 {
-        // println!("{left}-{right} [{path:?}] ({distance})");
-        // path.insert(current);
         let new_distance = if path.len() == distances.len() {
             distance
         } else {
             let mut new_distance = u64::MAX;
             for key in distances.keys() {
-                // println!("{left} / {key}");
-                // let a = distances[left][key];
-                // let b = distances[right][key];
-                //
                 if path.contains(key) {
                     continue;
                 }
-
-                match (distances[left].get(key), distances[right].get(key)) {
-                    (Some(a), Some(b)) => {
-                        if a < b {
-                            path.insert(key);
-                            new_distance =
-                                new_distance.min(walk(key, right, path, distance + a, distances));
-                            path.remove(key);
-                        } else {
-                            path.insert(key);
-                            new_distance =
-                                new_distance.min(walk(left, key, path, distance + b, distances));
-                            path.remove(key);
+                path.insert(key);
+                new_distance = new_distance.min(
+                    match (distances[left].get(key), distances[right].get(key)) {
+                        (Some(a), Some(b)) => {
+                            if a < b {
+                                walk(key, right, path, distance + a, distances)
+                            } else {
+                                walk(left, key, path, distance + b, distances)
+                            }
                         }
-                    }
-                    (Some(a), None) => {
-                        path.insert(key);
-                        new_distance =
-                            new_distance.min(walk(key, right, path, distance + a, distances));
-                        path.remove(key);
-                    }
-                    (None, Some(b)) => {
-                        path.insert(key);
-                        new_distance =
-                            new_distance.min(walk(left, key, path, distance + b, distances));
-                        path.remove(key);
-                    }
-                    _ => (),
-                }
+                        (Some(a), None) => walk(key, right, path, distance + a, distances),
+                        (None, Some(b)) => walk(left, key, path, distance + b, distances),
+                        _ => new_distance,
+                    },
+                );
+                path.remove(key);
             }
-            // distances[current]
-            //     .iter()
-            //     .filter_map(|(next, next_distance)| {
-            //         if path.contains(next) {
-            //             None
-            //         } else {
-            //             Some(walk(next, path, distance + next_distance, distances))
-            //         }
-            //     })
-            //     .min()
-            //     .unwrap()
             new_distance
         };
-        // path.remove(current);
         new_distance
     }
     let start = distances.keys().next().unwrap();
@@ -108,71 +82,47 @@ pub fn part1(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
 
 pub fn part2(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
     let content = fs::read_to_string(input_path)?;
-
-    let mut distances: HashMap<&str, HashMap<&str, u64>> = HashMap::with_capacity(8);
-    content.lines().for_each(|line| {
-        let mut parts = line.split(' ');
-        let (from, _, to, _, distance) = (
-            parts.next().unwrap(),
-            parts.next(),
-            parts.next().unwrap(),
-            parts.next(),
-            parts
-                .next()
-                .unwrap()
-                .parse::<u64>()
-                .expect("unable to parse distance"),
-        );
-
-        distances
-            .entry(from)
-            .or_default()
-            .entry(to)
-            .insert_entry(distance);
-        distances
-            .entry(to)
-            .or_default()
-            .entry(from)
-            .insert_entry(distance);
-    });
+    let distances = parse(&content);
 
     let mut path: HashSet<&str> = HashSet::with_capacity(8);
 
-    let mut bound = u64::MAX;
-
     fn walk<'a>(
-        current: &'a str,
+        left: &'a str,
+        right: &'a str,
         path: &mut HashSet<&'a str>,
         distance: u64,
         distances: &HashMap<&'a str, HashMap<&'a str, u64>>,
-        bound: &mut u64,
     ) -> u64 {
-        path.insert(current);
         let new_distance = if path.len() == distances.len() {
-            *bound = distance;
-            distance
-        } else if distance > *bound {
             distance
         } else {
-            distances[current]
-                .iter()
-                .filter_map(|(next, next_distance)| {
-                    if path.contains(next) {
-                        None
-                    } else {
-                        Some(walk(next, path, distance + next_distance, distances, bound))
-                    }
-                })
-                .max()
-                .unwrap()
+            let mut new_distance = u64::MIN;
+            for key in distances.keys() {
+                if path.contains(key) {
+                    continue;
+                }
+                path.insert(key);
+                new_distance = new_distance.max(
+                    match (distances[left].get(key), distances[right].get(key)) {
+                        (Some(a), Some(b)) => {
+                            if a > b {
+                                walk(key, right, path, distance + a, distances)
+                            } else {
+                                walk(left, key, path, distance + b, distances)
+                            }
+                        }
+                        (Some(a), None) => walk(key, right, path, distance + a, distances),
+                        (None, Some(b)) => walk(left, key, path, distance + b, distances),
+                        _ => new_distance,
+                    },
+                );
+                path.remove(key);
+            }
+            new_distance
         };
-        path.remove(current);
         new_distance
     }
-
-    Ok(distances
-        .keys()
-        .map(|start| walk(start, &mut path, 0, &distances, &mut bound))
-        .max()
-        .unwrap())
+    let start = distances.keys().next().unwrap();
+    path.insert(start);
+    Ok(walk(start, start, &mut path, 0, &distances))
 }
--- src/y2015/d10.rs
+++ src/y2015/d10.rs
@@ -1,12 +1,73 @@
-use std::fs::File;
-use std::io::{BufReader, Read};
+use std::fs;
+use std::mem;
 use std::path::PathBuf;
 
 pub fn part1(input_path: &PathBuf) -> Result<usize, Box<dyn std::error::Error>> {
-    let reader = BufReader::new(File::open(input_path)?);
-    Err("not implemented".into())
+    let mut sequence: Vec<u8> = fs::read(input_path)?
+        .iter()
+        .map(|c| c - b'0')
+        .filter(|v| *v < 10)
+        .collect();
+
+    let mut new = vec![];
+
+    for _ in 0..40 {
+        let mut i = sequence.iter();
+        let mut count: u8 = 0;
+        let mut last: &u8 = &u8::MAX;
+        while let Some(c) = i.next() {
+            if count >= 1 {
+                if c == last {
+                    count += 1;
+                    continue;
+                }
+                new.push(count);
+                new.push(*last);
+            }
+            last = c;
+            count = 1;
+        }
+        if count >= 1 {
+            new.push(count);
+            new.push(*last);
+        }
+        mem::swap(&mut sequence, &mut new);
+        new.clear();
+    }
+    Ok(sequence.len())
 }
 
-pub fn part2(_input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
-    Err("not implemented".into())
+pub fn part2(input_path: &PathBuf) -> Result<usize, Box<dyn std::error::Error>> {
+    let mut sequence: Vec<u8> = fs::read(input_path)?
+        .iter()
+        .map(|c| c - b'0')
+        .filter(|v| *v < 10)
+        .collect();
+
+    let mut new = vec![];
+
+    for _ in 0..50 {
+        let mut i = sequence.iter();
+        let mut count: u8 = 0;
+        let mut last: &u8 = &u8::MAX;
+        while let Some(c) = i.next() {
+            if count >= 1 {
+                if c == last {
+                    count += 1;
+                    continue;
+                }
+                new.push(count);
+                new.push(*last);
+            }
+            last = c;
+            count = 1;
+        }
+        if count >= 1 {
+            new.push(count);
+            new.push(*last);
+        }
+        mem::swap(&mut sequence, &mut new);
+        new.clear();
+    }
+    Ok(sequence.len())
 }
