9233febb85f889e51ee643367c9a31ad6a263082
Christoph Schniedermeier <zdxerr@gmail.com>
2025-12-22 18:19:22 +0100

    2015.11

--- src/y2015/d11.rs
+++ src/y2015/d11.rs
@@ -1,12 +1,70 @@
-use std::fs::File;
-use std::io::{BufReader, Read};
+use std::fs;
 use std::path::PathBuf;
 
-pub fn part1(input_path: &PathBuf) -> Result<usize, Box<dyn std::error::Error>> {
-    let reader = BufReader::new(File::open(input_path)?);
-    Err("not implemented".into())
+const I: u8 = b'i' - b'a';
+const O: u8 = b'o' - b'a';
+const L: u8 = b'l' - b'a';
+
+fn valid(password: &Vec<u8>) -> bool {
+    let mut straight = false;
+    let mut doubles = vec![];
+
+    for idx in 0..password.len() {
+        if matches!(password[idx], I | O | L) {
+            return false;
+        }
+        if idx < password.len() - 3
+            && password[idx + 2] == password[idx + 1] + 1
+            && password[idx + 1] == password[idx] + 1
+        {
+            straight = true;
+        }
+        if idx > 0 && password[idx - 1] == password[idx] && !doubles.contains(&(idx - 1)) {
+            doubles.push(idx);
+        }
+    }
+
+    straight && doubles.len() >= 2
 }
 
-pub fn part2(_input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
-    Err("not implemented".into())
+fn next(password: &mut Vec<u8>) {
+    for c in password.iter_mut().rev() {
+        *c = (*c + 1) % (b'z' - b'a' + 1);
+        if *c > 0 {
+            return;
+        }
+    }
+}
+
+fn format(password: &Vec<u8>) -> String {
+    password.iter().map(|c| char::from(c + b'a')).collect()
+}
+
+pub fn part1(input_path: &PathBuf) -> Result<String, Box<dyn std::error::Error>> {
+    let mut password: Vec<u8> = fs::read(input_path)?
+        .trim_ascii()
+        .iter()
+        .map(|c| c - b'a')
+        .collect();
+
+    loop {
+        next(&mut password);
+        if valid(&password) {
+            break;
+        }
+    }
+    Ok(format(&password))
+}
+
+pub fn part2(input_path: &PathBuf) -> Result<String, Box<dyn std::error::Error>> {
+    let mut password = part1(input_path)?.chars().map(|c| c as u8 - b'a').collect();
+    loop {
+        next(&mut password);
+        if valid(&password) {
+            break;
+        }
+    }
+    Ok(format(&password))
+
+    // Err("not implemented".into())
 }
