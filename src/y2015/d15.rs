75b991f15e658d6a5e0006dc84c8ee04241ea6ca
Christoph Schniedermeier <zdxerr@gmail.com>
2025-12-25 13:14:21 +0100

    2015.15.1

--- src/y2015/d15.rs
+++ src/y2015/d15.rs
@@ -1,100 +1,64 @@
 use std::fs;
 use std::path::PathBuf;
 
-type Ingredient = (i64, i64, i64, i64, i64);
+#[derive(Debug)]
+struct Ingredient(i64, i64, i64, i64, i64);
 
-// struct Ingredient {
-//     capacity: i64,
-//     durability: i64,
-//     flavor: i64,
-//     texture: i64,
-//     calories: i64,
-// }
-
-// impl Ingredient {
-//     fn new(line: &str) -> Self {
-//         let mut splitted = line.split(' ');
-//         Ingredient {
-//             capacity: splitted
-//                 .nth(2)
-//                 .unwrap()
-//                 .strip_suffix(',')
-//                 .unwrap()
-//                 .parse()
-//                 .unwrap(),
-//             durability: splitted
-//                 .nth(1)
-//                 .unwrap()
-//                 .strip_suffix(',')
-//                 .unwrap()
-//                 .parse()
-//                 .unwrap(),
-//             flavor: splitted
-//                 .nth(1)
-//                 .unwrap()
-//                 .strip_suffix(',')
-//                 .unwrap()
-//                 .parse()
-//                 .unwrap(),
-//             texture: splitted
-//                 .nth(1)
-//                 .unwrap()
-//                 .strip_suffix(',')
-//                 .unwrap()
-//                 .parse()
-//                 .unwrap(),
-//             calories: splitted.nth(1).unwrap().parse().unwrap(),
-//         }
-//     }
-// }
-
-pub fn part1(input_path: &PathBuf) -> Result<usize, Box<dyn std::error::Error>> {
+pub fn part1(input_path: &PathBuf) -> Result<i64, Box<dyn std::error::Error>> {
     let content = fs::read_to_string(input_path)?;
 
-    // Sprinkles: capacity 5, durability -1, flavor 0, texture 0, calories 5
     let ingredients: Vec<Ingredient> = content
         .lines()
         .map(|line| {
-            let mut splitted = line.split(' ');
-
-            (
-                splitted
-                    .nth(2)
-                    .unwrap()
-                    .strip_suffix(',')
-                    .unwrap()
-                    .parse()
-                    .unwrap(),
-                splitted
-                    .nth(1)
-                    .unwrap()
-                    .strip_suffix(',')
-                    .unwrap()
-                    .parse()
-                    .unwrap(),
-                splitted
-                    .nth(1)
-                    .unwrap()
-                    .strip_suffix(',')
-                    .unwrap()
-                    .parse()
-                    .unwrap(),
-                splitted
-                    .nth(1)
-                    .unwrap()
-                    .strip_suffix(',')
-                    .unwrap()
-                    .parse()
-                    .unwrap(),
-                splitted.nth(1).unwrap().parse().unwrap(),
+            let mut splitted = line.split([' ', ',']);
+            Ingredient(
+                splitted.nth(2).unwrap().parse().unwrap(),
+                splitted.nth(2).unwrap().parse().unwrap(),
+                splitted.nth(2).unwrap().parse().unwrap(),
+                splitted.nth(2).unwrap().parse().unwrap(),
+                splitted.nth(2).unwrap().parse().unwrap(),
             )
         })
         .collect();
 
-    dbg!(&ingredients);
-    //
+    fn next(ingredients: &[Ingredient], idx: usize, spoons: &mut [i64], left: i64) -> i64 {
+        if idx == ingredients.len() {
+            return spoons
+                .iter()
+                .zip(ingredients)
+                .map(|(spoon, ingredient)| *spoon * ingredient.0)
+                .sum::<i64>()
+                .max(0)
+                * spoons
+                    .iter()
+                    .zip(ingredients)
+                    .map(|(spoon, ingredient)| spoon * ingredient.1)
+                    .sum::<i64>()
+                    .max(0)
+                * spoons
+                    .iter()
+                    .zip(ingredients)
+                    .map(|(spoon, ingredient)| spoon * ingredient.2)
+                    .sum::<i64>()
+                    .max(0)
+                * spoons
+                    .iter()
+                    .zip(ingredients)
+                    .map(|(spoon, ingredient)| spoon * ingredient.3)
+                    .sum::<i64>()
+                    .max(0);
+        }
 
-    Err("not implemented".into())
+        (0..=left)
+            .map(|spoon| {
+                spoons[idx] = spoon;
+                next(ingredients, idx + 1, spoons, left - spoon)
+            })
+            .max()
+            .unwrap()
+    }
+
+    Ok(next(&ingredients, 0, &mut vec![0; ingredients.len()], 100))
 }
 
 pub fn part2(_input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
