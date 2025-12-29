1693bfb19ea74c97450ff49f4739b9d6c6c6b35c
Christoph Schniedermeier <zdxerr@gmail.com>
2025-12-29 13:03:13 +0100

    21.2

--- src/y2015/d21.rs
+++ src/y2015/d21.rs
@@ -30,7 +30,7 @@ const RINGS: [Item; 7] = [
     (80, 0, 3),  // Defense +3
 ];
 
-pub fn part1(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
+fn parse(input_path: &PathBuf) -> Result<(u64, u64, u64), Box<dyn std::error::Error>> {
     let content = fs::read_to_string(input_path)?;
 
     let mut stats = content
@@ -39,14 +39,17 @@ pub fn part1(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
         .flatten()
         .map(|(_, num)| num.parse::<u64>().unwrap());
 
-    let (boss_hit_points, boss_damage, boss_armor) =
-        if let (Some(hit_points), Some(damage), Some(armor)) =
-            (stats.next(), stats.next(), stats.next())
-        {
-            (hit_points, damage, armor)
-        } else {
-            panic!("unable to parse input");
-        };
+    if let (Some(hit_points), Some(damage), Some(armor)) =
+        (stats.next(), stats.next(), stats.next())
+    {
+        Ok((hit_points, damage, armor))
+    } else {
+        panic!("unable to parse input");
+    }
+}
+
+pub fn part1(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
+    let (boss_hit_points, boss_damage, boss_armor) = parse(input_path)?;
 
     let mut min_cost: Option<u64> = None;
 
@@ -55,29 +58,20 @@ pub fn part1(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
             for ring1 in RINGS {
                 for ring2 in RINGS {
                     if ring1 == ring2 && ring2 != RINGS[0] {
-                        continue;
+                        continue; // the same ring can only be equipiped once, except for no ring. 
                     }
                     let player_cost: u64 = [weapon.0, armor.0, ring1.0, ring2.0].iter().sum();
                     let player_damage: u64 = [weapon.1, armor.1, ring1.1, ring2.1].iter().sum();
                     let player_armor: u64 = [weapon.2, armor.2, ring1.2, ring2.2].iter().sum();
-                    // println!("{cost} {damage} {armor}");
                     let a = player_damage.checked_sub(boss_armor).unwrap_or(1).max(1);
                     let b = boss_damage.checked_sub(player_armor).unwrap_or(1).max(1);
 
                     let player_rounds = 100_u64.div_ceil(b);
                     let boss_rounds = boss_hit_points.div_ceil(a);
 
-                    // println!(
-                    //     "cost: {player_cost}, player rounds: {player_rounds}, boss rounds: {boss_rounds} {} ({a}, {b})",
-                    //     player_rounds >= boss_rounds
-                    // );
-
                     if player_rounds >= boss_rounds {
                         if let Some(ref mut min_cost) = min_cost {
                             if *min_cost > player_cost {
-                                // println!(
-                                //     "######################## {weapon:?},{armor:?},{ring1:?},{ring2:?} {min_cost:?} {player_cost}"
-                                // );
                                 *min_cost = player_cost;
                             }
                         } else {
@@ -89,9 +83,42 @@ pub fn part1(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
         }
     }
     min_cost.ok_or("no winning combination found".into())
-    // Err("not implemented".into())
 }
 
-pub fn part2(_input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
-    Err("not implemented".into())
+pub fn part2(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
+    let (boss_hit_points, boss_damage, boss_armor) = parse(input_path)?;
+    let mut max_cost: Option<u64> = None;
+
+    for weapon in WEAPONS {
+        for armor in ARMOR {
+            for ring1 in RINGS {
+                for ring2 in RINGS {
+                    if ring1 == ring2 && ring2 != RINGS[0] {
+                        continue; // the same ring can only be equipiped once, except for no ring. 
+                    }
+                    let player_cost: u64 = [weapon.0, armor.0, ring1.0, ring2.0].iter().sum();
+                    let player_damage: u64 = [weapon.1, armor.1, ring1.1, ring2.1].iter().sum();
+                    let player_armor: u64 = [weapon.2, armor.2, ring1.2, ring2.2].iter().sum();
+                    let effective_player_damage =
+                        player_damage.checked_sub(boss_armor).unwrap_or(1).max(1);
+                    let effective_boss_damage =
+                        boss_damage.checked_sub(player_armor).unwrap_or(1).max(1);
+
+                    let player_rounds = 100_u64.div_ceil(effective_boss_damage);
+                    let boss_rounds = boss_hit_points.div_ceil(effective_player_damage);
+
+                    if player_rounds < boss_rounds {
+                        if let Some(ref mut max_cost) = max_cost {
+                            if *max_cost < player_cost {
+                                *max_cost = player_cost;
+                            }
+                        } else {
+                            max_cost = Some(player_cost)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    max_cost.ok_or("no losing combination found".into())
 }
