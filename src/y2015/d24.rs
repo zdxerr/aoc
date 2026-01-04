899ecb9e31bc378e0f6cbe3eaa65dbf2911a6dd0
Christoph Schniedermeier <zdxerr@gmail.com>
2026-01-04 21:09:07 +0100

    16.1.1 solution.

--- src/y2015/d24.rs
+++ src/y2015/d24.rs
@@ -4,10 +4,6 @@ use std::path::PathBuf;
 
 /* Gosper's Hack */
 fn iter_u32_with_exactly_n_bits(n: u32) -> impl Iterator<Item = u32> {
-    // if n > 32 || n == 0 {
-    //     return std::iter::empty();
-    // }
-
     // Start with the smallest number having n bits set: 000...0111...1 (n ones)
     let mut subset = (1u64 << n) - 1;
     let limit = 1u64 << 32; // 2^32
@@ -31,25 +27,18 @@ fn iter_u32_with_exactly_n_bits(n: u32) -> impl Iterator<Item = u32> {
 
 pub fn part1(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
     let content = fs::read_to_string(input_path)?;
-    let mut packages: Vec<u64> = content.lines().map(|line| line.parse()).flatten().collect();
+    let packages: Vec<u64> = content.lines().map(|line| line.parse()).flatten().collect();
     let boundary = packages.iter().sum::<u64>().div_euclid(3);
-    packages.sort();
-    packages.reverse();
 
     let mut max_package_count = packages.len();
     let mut min_quantum_entaglement: u64 = packages.iter().product();
 
-    for x in 1.. {
-        for mut n in iter_u32_with_exactly_n_bits(x) {
-            let package_count = n.count_ones() as usize;
-            if package_count > max_package_count {
-                break;
-            }
+    for package_count in 1..packages.len() {
+        for mut n in iter_u32_with_exactly_n_bits(package_count as u32) {
+            let mut sum = 0;
+            let mut prod = 1;
 
             let start = n.trailing_zeros() as usize;
-            let mut sum = 0;
-            let mut set = HashSet::new();
-
             n >>= start;
 
             for index in start..packages.len() {
@@ -57,7 +46,7 @@ pub fn part1(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
                 if bit {
                     let p = packages[index];
                     sum += p;
-                    set.insert(p);
+                    prod *= p;
                 }
                 n >>= 1;
                 if n == 0 {
@@ -67,10 +56,10 @@ pub fn part1(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
 
             if sum == boundary {
                 max_package_count = package_count;
-                min_quantum_entaglement = min_quantum_entaglement.min(set.iter().product());
+                min_quantum_entaglement = min_quantum_entaglement.min(prod);
             }
         }
-        if x as usize >= max_package_count {
+        if package_count >= max_package_count {
             break;
         }
     }
@@ -80,25 +69,18 @@ pub fn part1(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
 
 pub fn part2(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
     let content = fs::read_to_string(input_path)?;
-    let mut packages: Vec<u64> = content.lines().map(|line| line.parse()).flatten().collect();
+    let packages: Vec<u64> = content.lines().map(|line| line.parse()).flatten().collect();
     let boundary = packages.iter().sum::<u64>().div_euclid(4);
-    packages.sort();
-    packages.reverse();
 
     let mut max_package_count = packages.len();
     let mut min_quantum_entaglement: u64 = packages.iter().product();
 
-    for x in 1.. {
-        for mut n in iter_u32_with_exactly_n_bits(x) {
-            let package_count = n.count_ones() as usize;
-            if package_count > max_package_count {
-                break;
-            }
+    for package_count in 1..packages.len() {
+        for mut n in iter_u32_with_exactly_n_bits(package_count as u32) {
+            let mut sum = 0;
+            let mut prod = 1;
 
             let start = n.trailing_zeros() as usize;
-            let mut sum = 0;
-            let mut set = HashSet::new();
-
             n >>= start;
 
             for index in start..packages.len() {
@@ -106,7 +88,7 @@ pub fn part2(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
                 if bit {
                     let p = packages[index];
                     sum += p;
-                    set.insert(p);
+                    prod *= p;
                 }
                 n >>= 1;
                 if n == 0 {
@@ -116,10 +98,10 @@ pub fn part2(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
 
             if sum == boundary {
                 max_package_count = package_count;
-                min_quantum_entaglement = min_quantum_entaglement.min(set.iter().product());
+                min_quantum_entaglement = min_quantum_entaglement.min(prod);
             }
         }
-        if x as usize >= max_package_count {
+        if package_count >= max_package_count {
             break;
         }
     }
--- src/y2016/d01.rs
+++ src/y2016/d01.rs
@@ -1,32 +1,82 @@
 use std::fs;
-use std::io::BufReader;
+use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 
-// --- Day 1: No Time for a Taxicab ---
-// Santa's sleigh uses a very high-precision clock to guide its movements, and the clock's oscillator is regulated by stars. Unfortunately, the stars have been stolen... by the Easter Bunny. To save Christmas, Santa needs you to retrieve all fifty stars by December 25th.
+pub fn part1(input_path: &PathBuf) -> Result<i64, Box<dyn std::error::Error>> {
+    let reader = BufReader::new(fs::File::open(input_path)?);
+    let mut direction: i8 = 0;
+    let mut position: (i64, i64) = (0, 0);
+    reader.split(b',').flatten().for_each(|p| {
+        let p = p.trim_ascii();
+        direction = (direction
+            + match p[0] {
+                b'R' => 1,
+                b'L' => -1,
+                _ => panic!("invalid turn: {:?} {:?}", p, str::from_utf8(p)),
+            })
+        .rem_euclid(4);
 
-// Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+        let number: i64 = p[1..].iter().fold(0, |n, c| n * 10 + (c - b'0') as i64);
 
-// You're airdropped near Easter Bunny Headquarters in a city somewhere. "Near", unfortunately, is as close as you can get - the instructions on the Easter Bunny Recruiting Document the Elves intercepted start here, and nobody had time to work them out further.
+        match direction {
+            0 => position.1 += number,
+            1 => position.0 += number,
+            2 => position.1 -= number,
+            3 => position.0 -= number,
+            _ => panic!("invalid direction: {direction}"),
+        }
+    });
 
-// The Document indicates that you should start at the given coordinates (where you just landed) and face North. Then, follow the provided sequence: either turn left (L) or right (R) 90 degrees, then walk forward the given number of blocks, ending at a new intersection.
-
-// There's no time to follow such ridiculous instructions on foot, though, so you take a moment and work out the destination. Given that you can only walk on the street grid of the city, how far is the shortest path to the destination?
-
-// For example:
-
-// Following R2, L3 leaves you 2 blocks East and 3 blocks North, or 5 blocks away.
-// R2, R2, R2 leaves you 2 blocks due South of your starting position, which is 2 blocks away.
-// R5, L5, R5, R3 leaves you 12 blocks away.
-// How many blocks away is Easter Bunny HQ?
-
-pub fn part1(input_path: &PathBuf) -> Result<usize, Box<dyn std::error::Error>> {
-    let _content = fs::read_to_string(input_path)?;
-    let _reader = BufReader::new(fs::File::open(input_path)?);
-
-    Err("not implemented".into())
+    Ok(position.0.abs() + position.1.abs())
 }
 
-pub fn part2(_input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
-    Err("not implemented".into())
+// Then, you notice the instructions continue on the back of the Recruiting Document. Easter Bunny HQ is actually at the first location you visit twice.
+
+// For example, if your instructions are R8, R4, R4, R8, the first location you visit twice is 4 blocks away, due East.
+
+// How many blocks away is the first location you visit twice?
+pub fn part2(input_path: &PathBuf) -> Result<i64, Box<dyn std::error::Error>> {
+    // let _content = fs::read_to_string(input_path)?;
+    let reader = BufReader::new(fs::File::open(input_path)?);
+
+    // let s: Vec<_> = "R5, L5, R5, R3".bytes().collect();
+    // let s: Vec<_> = "R2, R2, R2".bytes().collect();
+    // let s: Vec<_> = "R2, L3".bytes().collect();
+    // let s: Vec<_> = "L1, L1, L1, L1, L1, L1".bytes().collect();
+    let s: Vec<_> = "R8, R4, R4, R8".bytes().collect();
+
+    let mut direction: i8 = 0;
+    // let mut distance: i64 = 0;
+    let mut position: (i64, i64) = (0, 0);
+    // reader
+    // .split(b',')
+    // .flatten()
+    s.split(|c| *c == b',')
+        // .map(|c| c.trim_ascii())
+        .for_each(|p| {
+            let p = p.trim_ascii();
+            direction = (direction
+                + match p[0] {
+                    b'R' => 1,
+                    b'L' => -1,
+                    _ => panic!("invalid turn: {:?} {:?}", p, str::from_utf8(p)),
+                })
+            .rem_euclid(4);
+
+            let number: i64 = p[1..].iter().fold(0, |n, c| n * 10 + (c - b'0') as i64);
+
+            match direction {
+                0 => position.1 += number,
+                1 => position.0 += number,
+                2 => position.1 -= number,
+                3 => position.0 -= number,
+                _ => panic!("invalid direction: {direction}"),
+            }
+            println!(
+                "{}, {direction} {number} {position:?}",
+                str::from_utf8(p).unwrap()
+            );
+        });
+
+    Ok(position.0.abs() + position.1.abs())
 }
