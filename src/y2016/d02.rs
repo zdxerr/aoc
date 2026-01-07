1fd821fb38e615ba047b1985cbb28736b413d75d
Christoph Schniedermeier <zdxerr@gmail.com>
2026-01-08 00:26:18 +0100

    Next.

--- src/y2016/d02.rs
+++ src/y2016/d02.rs
@@ -35,29 +35,40 @@ pub fn part1(input_path: &PathBuf) -> Result<String, Box<dyn std::error::Error>>
     Ok(code)
 }
 
-pub fn part2(_input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
-    // You finally arrive at the bathroom (it's a several minute walk from the lobby so visitors
-    // can behold the many fancy conference rooms and water coolers on this floor) and go to punch
-    // in the code. Much to your bladder's dismay, the keypad is not at all like you imagined it.
-    // Instead, you are confronted with the result of hundreds of man-hours of
-    // bathroom-keypad-design meetings:
+pub fn part2(input_path: &PathBuf) -> Result<String, Box<dyn std::error::Error>> {
+    let reader = BufReader::new(fs::File::open(input_path)?);
+    let mut position: i8 = 5;
+    let mut code = String::new();
 
-    //     1
-    //   2 3 4
-    // 5 6 7 8 9
-    //   A B C
-    //     D
-    // You still start at "5" and stop when you're at an edge, but given the same instructions as
-    // above, the outcome is very different:
-
-    // You start at "5" and don't move at all (up and left are both edges), ending at 5.
-    // Continuing from "5", you move right twice and down three times (through "6", "7", "B", "D", "D"),
-    // ending at D.
-    // Then, from "D", you move five more times (through "D", "B", "C", "C", "B"), ending at B.
-    // Finally, after five more moves, you end at 3.
-    // So, given the actual keypad layout, the code would be 5DB3.
-
-    // Using the same instructions in your puzzle input, what is the correct bathroom code?
-
-    Err("not implemented".into())
+    reader.bytes().flatten().for_each(|c| {
+        position += match c {
+            b'U' => match position {
+                3 | 13 => -2,
+                6 | 7 | 8 | 10 | 11 | 12 => -4,
+                _ => 0,
+            },
+            b'D' => match position {
+                1 | 11 => 2,
+                2 | 3 | 4 | 6 | 7 | 8 => 4,
+                _ => 0,
+            },
+            b'L' => match position {
+                3 | 4 | 6 | 7 | 8 | 9 | 11 | 12 => -1,
+                _ => 0,
+            },
+            b'R' => match position {
+                2 | 3 | 5 | 6 | 7 | 8 | 10 | 11 => 1,
+                _ => 0,
+            },
+            b'\n' => {
+                code.push(match position {
+                    1..=9 => (position as u8 + b'0') as char,
+                    _ => ((position - 10) as u8 + b'A') as char,
+                });
+                0
+            }
+            _ => panic!("invalid instruction: {:?}", c as char),
+        }
+    });
+    Ok(code)
 }
--- src/y2016/d04.rs
+++ src/y2016/d04.rs
@@ -32,62 +32,35 @@ pub fn part1(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
         .sum())
 }
 
-// With all the decoy data out of the way, it's time to decrypt this list and get moving.
-
-// The room names are encrypted by a state-of-the-art shift cipher, which is nearly unbreakable
-// without the right software. However, the information kiosk designers at Easter Bunny HQ were
-// not expecting to deal with a master cryptographer like yourself.
-
-// To decrypt a room name, rotate each letter forward through the alphabet a number of times equal
-// to the room's sector ID. A becomes B, B becomes C, Z becomes A, and so on. Dashes become spaces.
-
-// For example, the real name for qzmt-zixmtkozy-ivhz-343 is very encrypted name.
-
-// What is the sector ID of the room where North Pole objects are stored?
-pub fn part2(input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
+pub fn part2(input_path: &PathBuf) -> Result<u32, Box<dyn std::error::Error>> {
     let reader = BufReader::new(fs::File::open(input_path)?);
-    reader.split(b'\n').flatten().find(|line| {
-        let mut parts = line[..line.len() - 7].rsplitn(2, |&c| c == b'-');
-        let (sector_id, code) = (
-            parts.next().expect("split line part 1"),
-            parts.next().expect("split line part 2"),
-        );
-        // println!(
-        //     "{:?}  {:?}",
-        //     std::str::from_utf8(code).unwrap(),
-        //     std::str::from_utf8(sector_id).unwrap()
-        // );
-        let sector_id: u32 = sector_id.iter().fold(0, |num, digit| {
-            // println!(" {} {}", *digit as char, num);
-            num * 10 + (digit - b'0') as u32
-        });
+    reader
+        .split(b'\n')
+        .flatten()
+        .find_map(|line| {
+            let mut parts = line[..line.len() - 7].rsplitn(2, |&c| c == b'-');
+            let (sector_id, code) = (
+                parts.next().expect("split line part 1"),
+                parts.next().expect("split line part 2"),
+            );
+            let sector_id: u32 = sector_id
+                .iter()
+                .fold(0, |num, digit| num * 10 + (digit - b'0') as u32);
 
-        // println!("{sector_id:?} {code:?}");
-        let new = String::from_utf8(
-            code.iter()
-                .map(|c| match c {
-                    b'-' => b' ',
-                    _ => ((c - b'a') as u32 + sector_id).rem_euclid(26) as u8 + b'a',
-                })
-                .collect::<Vec<u8>>(),
-        )
-        .unwrap();
-        if new.contains("northpole object storage") {
-            println!("{new:?} {sector_id}");
-        }
-
-        // line[..line.len() - 7].iter().rev().for_each(|c| {
-        //     let idx = (c - b'a') as usize;
-        //     let num = c - b'0';
-
-        //     // if let Some((count, _)) = count.get_mut(idx) {
-        //     //     *count += 1;
-        //     // } else if num < 10 {
-        //     //     sector_id = sector_id * 10 + num as u64;
-        //     // }
-        // });
-        false
-    });
-
-    Err("not implemented".into())
+            let new = String::from_utf8(
+                code.iter()
+                    .map(|c| match c {
+                        b'-' => b' ',
+                        _ => ((c - b'a') as u32 + sector_id).rem_euclid(26) as u8 + b'a',
+                    })
+                    .collect::<Vec<u8>>(),
+            )
+            .unwrap();
+            if new.contains("northpole object storage") {
+                Some(sector_id)
+            } else {
+                None
+            }
+        })
+        .ok_or("sector id not found".into())
 }
