52b36b5977f16456a6c164e7c42ce95dda31fb76
Christoph Schniedermeier <zdxerr@gmail.com>
2026-01-15 00:17:28 +0100

    Refactor.

--- y2016/src/d12.rs
+++ y2016/src/d12.rs
@@ -42,37 +42,25 @@ fn next_value(bytes: &[u8]) -> Value {
     }
 }
 
-pub fn part1(input_path: &PathBuf) -> Result<i64, Box<dyn std::error::Error>> {
-    // let content = fs::read_to_string(input_path)?;
+pub fn execute(
+    input_path: &PathBuf,
+    mut register: [i64; 4],
+) -> Result<i64, Box<dyn std::error::Error>> {
     let reader = BufReader::new(fs::File::open(input_path)?);
-
-    // let mut a = None;
 
     let program: Vec<Command> = reader
         .split(b'\n')
         .flatten()
-        .map(|line| {
-            println!(
-                "{:?} {:?} __ {:?}",
-                str::from_utf8(&line),
-                &line[3..line.len() - 1],
-                next_value(&line[3..line.len() - 1])
-            );
-
-            // line[3..].split_once(b' ');
-            match &line[0..3] {
-                b"cpy" => Command::CPY(next_value(&line[3..]), next_value(&line[6..])),
-                b"inc" => Command::INC(next_value(&line[3..])),
-                b"dec" => Command::DEC(next_value(&line[3..])),
-                b"jnz" => Command::JNZ(next_value(&line[3..]), next_value(&line[6..])),
-                _ => panic!("PANIC"),
-            }
+        .map(|line| match &line[0..3] {
+            b"cpy" => Command::CPY(next_value(&line[3..]), next_value(&line[6..])),
+            b"inc" => Command::INC(next_value(&line[3..])),
+            b"dec" => Command::DEC(next_value(&line[3..])),
+            b"jnz" => Command::JNZ(next_value(&line[3..]), next_value(&line[6..])),
+            _ => panic!("PANIC"),
         })
         .collect();
 
-    dbg!(&program);
     let mut pos = 0;
-    let mut register = [0; 4];
 
     while let Some(cmd) = program.get(pos) {
         match cmd {
@@ -86,14 +74,14 @@ pub fn part1(input_path: &PathBuf) -> Result<i64, Box<dyn std::error::Error>> {
             Command::DEC(Value::Register(index)) => register[*index] -= 1,
             Command::JNZ(Value::Register(index), Value::Integer(jump)) => {
                 if register[*index] != 0 {
-                    println!("JNZ {pos} {jump}, {}", pos + *jump as usize);
+                    // println!("JNZ {pos} {jump}, {}", pos + *jump as usize);
                     pos += *jump as usize;
                     continue;
                 }
             }
             Command::JNZ(Value::Integer(value), Value::Integer(jump)) => {
                 if *value != 0 {
-                    println!("JNZ {pos} {jump}, {}", pos + *jump as usize);
+                    // println!("JNZ {pos} {jump}, {}", pos + *jump as usize);
                     pos += *jump as usize;
                     continue;
                 }
@@ -102,17 +90,13 @@ pub fn part1(input_path: &PathBuf) -> Result<i64, Box<dyn std::error::Error>> {
         }
         pos += 1;
     }
-    // for c in program {
-    //     println!("{c:?}");
-    // }
     Ok(register[0])
-    // Err("not implemented".into())
 }
 
-pub fn part2(_input_path: &PathBuf) -> Result<u64, Box<dyn std::error::Error>> {
-    // As you head down the fire escape to the monorail, you notice it didn't start; register c
-    // needs to be initialized to the position of the ignition key.
+pub fn part1(input_path: &PathBuf) -> Result<i64, Box<dyn std::error::Error>> {
+    execute(input_path, [0; 4])
+}
 
-    // If you instead initialize register c to be 1, what value is now left in register a?
-    Err("not implemented".into())
+pub fn part2(input_path: &PathBuf) -> Result<i64, Box<dyn std::error::Error>> {
+    execute(input_path, [0, 0, 1, 0])
 }
