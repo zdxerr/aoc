3ab1db3f7ef835f5e3b1a1ed2eda796f239e3741
Christoph Schniedermeier <zdxerr@gmail.com>
2026-01-16 16:01:07 +0100

    Attempt 2016.11.

--- y2016/src/d11.rs
+++ y2016/src/d11.rs
@@ -2,97 +2,130 @@ use std::collections::VecDeque;
 use std::fs;
 use std::path::PathBuf;
 
-// #[derive(Debug)]
-// enum Item<'a> {
-//     Generator(&'a str),
-//     Microchip(&'a str),
-// }
-#[derive(Debug, Clone)]
-enum Item<'a> {
-    Generator(&'a str),
-    Microchip(&'a str),
-}
+type Floors<T> = Vec<(Vec<T>, Vec<T>)>;
+type State<T> = (usize, usize, Floors<T>);
 
-// type State<'a T> = (usize, Vec<Vec<&'a Item<'a>>>);
-type Floors<T> = Vec<Vec<T>>;
-type State<T> = (usize, Floors<T>);
+fn valid(floors: &Floors<&str>) -> bool {
+    for (generators, microchips) in floors {
+        for microchip in microchips {
+            if !generators.is_empty() && !generators.iter().any(|generator| generator.eq(microchip))
+            {
+                return false;
+            }
+        }
+    }
+    // for microchip in state.2[state.1].1 {
+    //     for generator in state.2[state.1].0 {
+    //         if microchip == generator {
+    //             return false;
+    //         }
+    //     }
+    // }
+    true
+}
 
 pub fn part1(input_path: &PathBuf) -> Result<usize, Box<dyn std::error::Error>> {
     // let content = fs::read_to_string(input_path)?;
-    let mut queue: VecDeque<State<&Item>> = VecDeque::with_capacity(1000);
     let content = fs::read_to_string(r"input/y2016/d11/test.txt")?;
 
-    let init_floors: Floors<Item> = content
+    let init_floors: Floors<&str> = content
         .lines()
         .map(|line| {
+            let (mut generators, mut microchips) = (Vec::new(), Vec::new());
             let splitted: Vec<&str> = line.split_whitespace().collect();
             splitted
-                .iter()
-                .enumerate()
-                .filter_map(|(pos, s)| {
-                    s.starts_with("generator")
-                        .then(|| Item::Generator(splitted[pos - 1]))
-                        .or_else(|| {
-                            s.starts_with("microchip").then(|| {
-                                Item::Microchip(
-                                    splitted[pos - 1]
-                                        .split_once('-')
-                                        .unwrap_or((splitted[pos - 1], ""))
-                                        .0,
-                                )
-                            })
-                        })
-                })
-                .collect()
+                .windows(2)
+                .for_each(|words| match *&words[1].trim_end_matches(['.', ',']) {
+                    "generator" => generators.push(words[0]),
+                    "microchip" => microchips.push(words[0].trim_end_matches("-compatible")),
+                    _ => (),
+                });
+            (generators, microchips)
         })
         .collect();
-    println!();
-    queue.push_back((
-        0,
-        init_floors
+    let mut queue: VecDeque<State<&str>> = VecDeque::with_capacity(1000);
+    queue.push_back((0, 0, init_floors));
+
+    while let Some((step, floor, floors)) = queue.pop_front() {
+        if !valid(&floors) {
+            continue;
+        }
+        // println!();
+        // for (floor_index, (generators, microchips)) in floors.iter().enumerate().rev() {
+        //     print!(
+        //         "{floor_index:01} {:1} ",
+        //         if floor_index == floor { '#' } else { ' ' }
+        //     );
+        //     for microchip in microchips {
+        //         print!("[{microchip}] ");
+        //     }
+        //     for generator in generators {
+        //         print!("{{{generator}}} ");
+        //     }
+        //     println!();
+        // }
+
+        if floors[0..floors.len() - 1]
             .iter()
-            .map(|floor| floor.iter().collect())
-            .collect(),
-    ));
-
-    while let Some((floor, floors)) = queue.pop_front() {
-        println!("Current floor: {floor} {:?}", floors[floor]);
-
-        let current_floor = &floors[floor];
-
-        current_floor
-            .iter()
-            .enumerate()
-            .filter(|(_, item)| matches!(item, Item::Microchip(_)))
-            .for_each(|(index, microchip)| {
-                if (0..init_floors.len() - 1).contains(&floor) {
-                    let next = floor + 1;
-                    let mut next_floors = floors.clone();
-                    next_floors[floor].remove(index);
-                    next_floors[next].push(microchip);
-                    println!("NEXT+ {next} {next_floors:?}");
+            .map(|(generators, microchips)| generators.is_empty() && microchips.is_empty())
+            .all(|c| c)
+        {
+            println!();
+            for (floor_index, (generators, microchips)) in floors.iter().enumerate().rev() {
+                print!(
+                    "{floor_index:01} {:1} ",
+                    if floor_index == floor { '#' } else { ' ' }
+                );
+                for microchip in microchips {
+                    print!("[{microchip}] ");
                 }
-                if (1..init_floors.len()).contains(&floor) {
-                    let next = floor - 1;
-                    let mut next_floors = floors.clone();
-                    next_floors[floor].remove(index);
-                    next_floors[next].push(microchip);
-                    println!("NEXT- {next} {next_floors:?}");
+                for generator in generators {
+                    print!("{{{generator}}} ");
+                }
+                println!();
+            }
+            return Ok(step);
+        }
+        let microchips_len = floors[floor].1.len();
+        for microchip_index0 in 0..microchips_len {
+            let next_floor = floor + 1;
+            if (0..floors.len()).contains(&next_floor) {
+                let mut next_floors = floors.clone();
+                // dbg!(&next_floors);
+                // println!("{:?}", next_floors[floor].0);
+                let microchip0 = next_floors[floor].1.remove(microchip_index0);
+                next_floors[next_floor].1.push(microchip0);
+
+                if let Some(generator_index) =
+                    next_floors[floor].0.iter().position(|s| s == &microchip0)
+                {
+                    let generator = next_floors[floor].0.remove(generator_index);
+                    next_floors[next_floor].0.push(generator);
                 }
 
-                // [floor.]
-                // println!("{element}");
-            });
+                queue.push_back((step + 1, next_floor, next_floors));
+
+                for microchip_index1 in microchip_index0 + 1..microchips_len {
+                    let mut next_floors = floors.clone();
+                    let microchip1 = next_floors[floor].1.remove(microchip_index1); // this will change the index of following elements
+                    let microchip0 = next_floors[floor].1.remove(microchip_index0);
+                    next_floors[next_floor].1.push(microchip1);
+                    next_floors[next_floor].1.push(microchip0);
+
+                    queue.push_back((step + 1, next_floor, next_floors));
+                }
+            }
+            let next_floor = floor - 1;
+            if (0..floors.len()).contains(&next_floor) {
+                let mut next_floors = floors.clone();
+                let microchip0 = next_floors[floor].1.remove(microchip_index0);
+                next_floors[next_floor].1.push(microchip0);
+                queue.push_back((step + 1, next_floor, next_floors));
+            }
+        }
     }
 
     println!();
-
-    // How to encode the current state in an easy to copy way?
-
-    // let fl0: &[Vec<&str>; floors.len()] = [["test"]; 4];
-
-    // let n = fl0.clone();
-    // let v = floors.clone_from_slice(src);
 
     Err("not implemented".into())
 }
