89e24a5c8e2bd7221b64189cb9b6f0296e70134e
Christoph Schniedermeier <zdxerr@gmail.com>
2026-01-23 00:28:22 +0100

    D16.2

--- y2016/src/d16.rs
+++ y2016/src/d16.rs
@@ -2,9 +2,25 @@ use std::fs;
 use std::io::BufReader;
 use std::path::PathBuf;
 
+const DISK_LENGHT: usize = 272;
+
 pub fn part1(input_path: &PathBuf) -> Result<usize, Box<dyn std::error::Error>> {
-    let _content = fs::read_to_string(input_path)?;
-    let _reader = BufReader::new(fs::File::open(input_path)?);
+    let content = fs::read_to_string(input_path)?;
+    // let _reader = BufReader::new(fs::File::open(input_path)?);
+    //
+    //
+    let mut content: Vec<bool> = content.chars().map(|c| c == '1').collect();
+    // println!("{content}");
+    let s: String = content.iter().map(|&b| if b { '1' } else { '0' }).collect();
+    println!("{s:?}");
+    // println!("{:0b}", x.concat());
+    //
+    //
+    // let sl = &content[..];
+    // let next: Vec<bool> = content.iter().rev().map(|b| !b).collect();
+    // let new = content.extend_from_slice(&[false]).extend(next).collect();
+    // let s: String = content.iter().map(|&b| if b { '1' } else { '0' }).collect();
+    // println!("{s:?}");
 
     Err("not implemented".into())
 }
