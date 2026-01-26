5dd580558de2d7722b85053d5d0afce274864b2c
Christoph Schniedermeier <zdxerr@gmail.com>
2026-01-26 12:54:38 +0100

    Improve 16.18 performance.

--- y2016/src/d18.rs
+++ y2016/src/d18.rs
@@ -1,53 +1,5 @@
 use std::fs;
-use std::io::Read;
 use std::path::PathBuf;
-
-fn _print_traps(traps: &[bool], row_len: &usize) {
-    for idx in (0..traps.len()).step_by(*row_len) {
-        println!(
-            "{}",
-            traps[idx..traps.len().min(idx + row_len)]
-                .iter()
-                .map(|trap| if *trap { '^' } else { '.' })
-                .collect::<String>()
-        );
-    }
-}
-
-pub fn part1(input_path: &PathBuf) -> Result<usize, Box<dyn std::error::Error>> {
-    let row0 = fs::read_to_string(input_path)?.trim().to_string();
-    let rows = 40;
-
-    // let row0 = ".^^.^.^^^^";
-    let row_len = row0.len();
-    let mut traps: Vec<bool> = row0.bytes().map(|c| c == b'^').collect();
-
-    for idx in row_len..row_len * rows {
-        let a = if idx.rem_euclid(row_len) == 0 {
-            false
-        } else {
-            traps[idx - row_len - 1]
-        };
-        let b = traps[idx - row_len];
-        let c = if idx.rem_euclid(row_len) == row_len - 1 {
-            false
-        } else {
-            traps[idx - row_len + 1]
-        };
-
-        traps.push(match (a, b, c) {
-            (true, true, false) => true,
-            (false, true, true) => true,
-            (true, false, false) => true,
-            (false, false, true) => true,
-            _ => false,
-        });
-    }
-    // println!();
-    // _print_traps(&traps, &row_len);
-
-    Ok(traps.iter().filter(|&&trap| !trap).count())
-}
 
 fn solve(input_path: &PathBuf, rows: usize) -> Result<usize, Box<dyn std::error::Error>> {
     let mut row0: Vec<bool> = fs::read(input_path)?
@@ -71,31 +23,27 @@ fn solve(input_path: &PathBuf, rows: usize) -> Result<usize, Box<dyn std::error:
             if idx == 0 {
                 std::mem::swap(&mut previos_row, &mut current_row);
             }
-            let a = if idx == 0 {
-                false
-            } else {
-                previos_row[idx - 1]
-            };
-            let b = previos_row[idx];
-            let c = if idx == row_len - 1 {
-                false
-            } else {
-                previos_row[idx + 1]
-            };
-            current_row[idx] = match (a, b, c) {
-                (true, true, false)
-                | (false, true, true)
-                | (true, false, false)
-                | (false, false, true) => true,
+            let triple = (
+                if idx == 0 {
+                    false
+                } else {
+                    previos_row[idx - 1]
+                },
+                previos_row[idx],
+                if idx == row_len - 1 {
+                    false
+                } else {
+                    previos_row[idx + 1]
+                },
+            );
+            current_row[idx] = match triple {
+                (false, _, true) | (true, _, false) => true,
                 _ => {
                     count += 1;
                     false
                 }
             };
         }
-        // if idx == 0 {
-        //     std::mem::swap(&mut previos_row, &mut current_row);
-        // }
         // println!(
         //     "{}   {count}",
         //     String::from_utf8_lossy(&previos_row).as_ref()
@@ -105,40 +53,10 @@ fn solve(input_path: &PathBuf, rows: usize) -> Result<usize, Box<dyn std::error:
     Ok(count)
 }
 
+pub fn part1(input_path: &PathBuf) -> Result<usize, Box<dyn std::error::Error>> {
+    solve(input_path, 40)
+}
+
 pub fn part2(input_path: &PathBuf) -> Result<usize, Box<dyn std::error::Error>> {
     solve(input_path, 400_000)
-    // let row0 = fs::read_to_string(input_path)?.trim().to_string();
-    // let rows = 400_000;
-
-    // // let row0 = ".^^.^.^^^^";
-    // let row_len = row0.len();
-    // let mut traps: Vec<bool> = row0.bytes().map(|c| c == b'^').collect();
-
-    // for idx in row_len..row_len * rows {
-    //     let a = if idx.rem_euclid(row_len) == 0 {
-    //         false
-    //     } else {
-    //         traps[idx - row_len - 1]
-    //     };
-    //     let b = traps[idx - row_len];
-    //     let c = if idx.rem_euclid(row_len) == row_len - 1 {
-    //         false
-    //     } else {
-    //         traps[idx - row_len + 1]
-    //     };
-
-    //     traps.push(match (a, b, c) {
-    //         (true, true, false) => true,
-    //         (false, true, true) => true,
-    //         (true, false, false) => true,
-    //         (false, false, true) => true,
-    //         _ => false,
-    //     });
-    // }
-    // // println!();
-    // // _print_traps(&traps, &row_len);
-    // //
-    // // println!("Size: {}", traps.len() * std::mem::size_of::<bool>());
-
-    // Ok(traps.iter().filter(|&&trap| !trap).count())
 }
